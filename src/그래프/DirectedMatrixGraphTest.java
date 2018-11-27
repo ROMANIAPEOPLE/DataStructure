690db6f5eab0c0051920033d4a7e3f93aca7243a
ROMANIAPEOPLE <rlgur405@gmail.com>
2018-11-27 23:44:02 +0900

    Update DirectedMatrixGraphTest.java

--- src/그래프/DirectedMatrixGraphTest.java
+++ src/그래프/DirectedMatrixGraphTest.java
@@ -1,65 +1,42 @@
 package 그래프;
+//방향 그래프를 생성하고 이용하는 프로그램
 
 import java.util.Scanner;
 
-/*
- * 파일명: DirectedListGraphTest.java
- * 작성일: 2018.11.22
- * 작성자: 정기혁
-* 설명: 방향 그래프를 생성하고 이용하는 프로그램
-*/
 class DirectedMatrixGraph {
-	public class Node {
-		int vertex;
-		Node link;
 
-		public Node(int vertex) {
-			this.vertex = vertex;
-		}
-	}
-
-	private Node[] list;
-	private int n;
-
+	private int matrix[][];
+	private int n; //정점수
+	
 	public DirectedMatrixGraph(int n) {
-		this.n = n;
-		list = new Node[n];
+		this.n=n;
+		matrix = new int[n][n];
 	}
-
+	
 	public boolean hasEdge(int v1, int v2) {
-		Node temp = list[v1];
-		while (temp != null) {
-			if (temp.vertex == v2) {
-				return true;
-			} else
-				temp = temp.link;
+		if(matrix[v1][v2] == 1) {
+			return true;
 		}
-
 		return false;
 	}
-
-	public void addEdge(int v1, int v2) {
-		if (hasEdge(v1, v2)) {
-			System.out.println("간선 삽입 오류 - 이미 존재하는 간선입니다" + "<" + v1 + ',' + v2 + ">");
-		} else if (v1 < 0 || v1 >= n || v2 < 0 || v2 >= n) {
-			System.out.println("간선 삽입 오류 - 잘못된 정점 번호입니다." + "<" + v1 + ',' + v2 + ">");
-		} else {
-
-			Node t = new Node(v2);
-			if (list[v1] == null) {
-				list[v1] = t;
-			} else {
-//**방향을 가지는 그래프는 addLast를 이용해야함.
-				list[v1].link = t;
-			}
+	
+	public void addEdge(int v1 , int v2) {
+		if(v1 <0 || v1>n-1 || v2<0 || v2>n-1) {
+			System.out.println("잘못된 정점");
+		}
+		else if(hasEdge(v1,v2)) {
+			System.out.println("이미 존재하는 간선");
+		}
+		else { //삽입 가능
+			matrix[v1][v2] = 1;
 		}
 	}
-
+	
 	public void printAdjacentVertices(int v) {
-		Node tmp = list[v];
-		while (tmp != null) {
-			System.out.print(tmp.vertex + " ");
-			tmp = tmp.link;
+		for(int i=0; i<matrix.length; i++) {
+			if(matrix[v][i]==1) {
+				System.out.print(i + " ");
+			}
 		}
 		System.out.println();
 	}
